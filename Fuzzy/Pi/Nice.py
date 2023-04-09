c6fce46717a40854ac03eccbeeea7b98cf96e496
Grey Liedtke <grey.liedtke@gmail.com>
2023-04-09 14:13:52 -0700

    sim mode

--- Fuzzy/Pi/Nice.py
+++ Fuzzy/Pi/Nice.py
@@ -1,45 +1,76 @@
 from nicegui import ui
 import json
-from motor import motor_cmd
-from mpu_6050 import read_theta
+from fuzz_control import motor_fuzz
+real=False
+if real:
+    from motor import motor_cmd
+    from mpu_6050 import read_theta
 
 motor = {
     "dir": 0,
     "speed": 0
  }
 
-def funcer(angle):
-    print(f'servo angle:{angle}')
-    pass
-
 def send_motor():
-    motor["dir"] = dir_toggle.value
-    motor["speed"] = motor_speed_slider.value
-    motor_cmd(motor["dir"], float(motor["speed"]/100))
+    if motor_speed_slider.value<0: motor["dir"]=-1
+    elif motor_speed_slider.value>0: motor["dir"]=1
+    else: motor["dir"]=0
+
+    motor_dir.value = motor["dir"]
+    motor["speed"] = abs(motor_speed_slider.value)
+    motor_speed.value = motor["speed"]
+
+    if real:
+        motor_cmd(motor["dir"], float(motor["speed"]/100))
 
 def read_acc():
-    th,thd = read_theta()
+    if real:
+        th,thd = read_theta()
+    else:
+        th,thd = sim_theta_slider.value, sim_theta_slider.value
     theta_x.value = round(th, 1)
     theta_x_dot.value = round(thd, 1)
 
+def control_loop():
+    
+    if control_mode.value == 'Manual':
+        pass
+    
+    elif control_mode.value == 'Fuzzy':
+        # get acc value
+        # decide on motor output based off reading
+        motor_speed_slider.value = motor_fuzz(theta_x.value)
+    
+    send_motor()
+
+def stop_motor():
+    motor_speed_slider.value = 0
+    send_motor()
 
 # UI
 ui.markdown("## Pendulum Control")
 
 # ---------------------------------------------------
 with ui.row():
-    with ui.card() as card:
-        card.classes('w-full')
+
+    with ui.card():
+        control_mode = ui.toggle(['Manual', 'Fuzzy'], value='Manual')
+    with ui.card().classes('w-full'):
         ui.timer(interval=0.5, callback=read_acc)
+        ui.timer(interval=0.5, callback=control_loop)
         ui.markdown("### Accelerometer")
         theta_x = ui.number("theta")
         theta_x_dot = ui.number("theta_dot")
 
-    with ui.card() as card:
-        ui.markdown("### Motor")
+        sim_theta_slider = ui.slider(min=-45, max=45, value=0)
 
-        dir_toggle = ui.toggle(['OFF','FWD', 'BWD'], on_change=lambda: send_motor())
-        motor_speed_slider = ui.slider(min=0, max=100, value=0, on_change=lambda: send_motor())
+
+    with ui.card().classes('w-full'):
+        ui.markdown("### Motor")
+        motor_dir = ui.number("dir")
+        motor_speed = ui.number("%")
+        ui.button('OFF', on_click=lambda: stop_motor())
+        motor_speed_slider = ui.slider(min=-100, max=100, value=0, on_change=lambda: send_motor())
 
 
 # running the page
--- Fuzzy/Pi/fuzz_control.py
+++ Fuzzy/Pi/fuzz_control.py
@@ -16,35 +16,34 @@ th1 = ctrl.Antecedent(th1_range, 'th1')
 # Auto-membership function population is possible with .automf(3, 5, or 7)
 # accelerometer memberships
 # theta leaning
-th1['Right Leaning'] = fuzz.trimf(th1_range, [-90, -90, 0])# fuzz.sigmoid(th1_range, 180, 2)
-th1['Balanced'] = fuzz.trimf(th1_range, [-70, 0, 70])# fuzz.sigmoid(th1_range, 180, 2)
-th1['Left Leaning'] = fuzz.trimf(th1_range, [0, 90, 90])
+th1['Right Leaning'] = fuzz.trimf(th1_range, [-45, -45, 0])# fuzz.sigmoid(th1_range, 180, 2)
+th1['Balanced'] = fuzz.trimf(th1_range, [-30, 0, 30])# fuzz.sigmoid(th1_range, 180, 2)
+th1['Left Leaning'] = fuzz.trimf(th1_range, [0, 45, 45])
 
 # theta rotation
 # th1_d['RR'] = fuzz.gaussmf(th1_d_range, -5, 1.5)
 # th1_d['LR'] = fuzz.gaussmf(th1_d_range, 5, 1.5)
 
+motor_range = np.arange(-100, 100, 1)
+motor = ctrl.Consequent(motor_range, 'motor')
 
-servo_range = np.arange(0, 181, 1)
-servo = ctrl.Consequent(servo_range, 'servo')
-
-servo['theta_cmd_right'] = fuzz.trimf(servo_range, [0, 0, 75])
-servo['theta_cmd_left'] = fuzz.trimf(servo_range, [105, 180, 180])
-servo['theta_cmd_middle'] = fuzz.gaussmf(servo_range, 90, 30)
+motor['theta_cmd_right'] = fuzz.trimf(motor_range, [-100, -100, 0])
+motor['theta_cmd_left'] = fuzz.trimf(motor_range, [0, 100, 100])
+motor['theta_cmd_middle'] = fuzz.gaussmf(motor_range, 0, 30)
 
 
 
 # -----------------------------------------------------------------------------------
 # creating rules and control system
-rule1 = ctrl.Rule(th1['Right Leaning'], servo['theta_cmd_right'])
-rule2 = ctrl.Rule(th1['Left Leaning'], servo['theta_cmd_left'])
-rule3 = ctrl.Rule(th1['Balanced'], servo['theta_cmd_middle'])
-servo_rules = ctrl.ControlSystem([rule1, rule2, rule3])
-servo_control = ctrl.ControlSystemSimulation(servo_rules)
+rule1 = ctrl.Rule(th1['Right Leaning'], motor['theta_cmd_right'])
+rule2 = ctrl.Rule(th1['Left Leaning'], motor['theta_cmd_left'])
+rule3 = ctrl.Rule(th1['Balanced'], motor['theta_cmd_middle'])
+motor_rules = ctrl.ControlSystem([rule1, rule2, rule3])
+motor_control = ctrl.ControlSystemSimulation(motor_rules)
 
 
-def servo_reponse(theta_input):
-    servo_control.input['th1'] = theta_input
-    servo_control.compute()
-    servo_send_angle = servo_control.output['servo']
-    return servo_send_angle
+def motor_fuzz(theta_input):
+    motor_control.input['th1'] = theta_input
+    motor_control.compute()
+    motor_signal = motor_control.output['motor']
+    return motor_signal
