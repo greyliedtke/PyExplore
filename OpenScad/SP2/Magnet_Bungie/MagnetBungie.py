3ffca2d47f3688ac5355d473ec46dea0cad2f41c
Grey <grey.liedtke@gmail.com>
2023-05-08 20:44:29 -0700

    servom

--- OpenScad/SP2/Magnet_Bungie/MagnetBungie.py
+++ OpenScad/SP2/Magnet_Bungie/MagnetBungie.py
@@ -6,9 +6,9 @@ script to create pi zero mounting
 import solid2 as ps
 
 # ------------------- main dimensions ---------------------------------
-mag_od = 8
-mag_h = 7
-bungie_od = 5
+mag_od = 9
+mag_h = 4
+bungie_od = 6
 thick = 2
 d_height = mag_h+bungie_od+2*thick
 hh = 30
@@ -19,7 +19,7 @@ def tube(od, id, height):
 
 
 # ------------------- Base magnet ------------------------------------
-p_base = tube(mag_od, mag_od-thick, thick)
+p_base = tube(mag_od, mag_od-2*thick, thick)
 p_tube = tube(mag_od+2*thick, mag_od, d_height)
 b_hole = ps.cylinder(d=bungie_od, h=hh).rotateX(90).forward(hh/2).up(d_height-bungie_od)
 
