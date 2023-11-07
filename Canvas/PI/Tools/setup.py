5b2457cf594853765f79265f4867da3446abb571
greyscale <grey.liedtke@scaled-power.com>
2023-11-06 16:56:45 -0800

    clock logic

--- Canvas/PI/Tools/setup.py
+++ Canvas/PI/Tools/setup.py
@@ -4,10 +4,10 @@ script to control most of configuration...
 
 brightness = 1
 
-color_time = [0, 0, 10]
-color_sec = [0, 1, 0]
-color_date = [10, 0, 0]
-color_background = [2, 2, 2]
+color_time = [94, 225, 235]
+color_sec = [126, 237, 165]
+color_date = [255, 193, 99]
+color_background = [145, 144, 142]
 color_off = [0, 0, 0]
 
 temp_refresh = 60 * 60  # seconds
