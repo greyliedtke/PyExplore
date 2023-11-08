445df46e4d66309bf8967458103d4bfefcef9bfa
Grey <grey.liedtke@gmail.com>
2023-11-08 09:57:53 -0800

    cccc

--- Canvas/PI/clock.py
+++ Canvas/PI/clock.py
@@ -9,13 +9,14 @@ import time
 import Tools.setup as pc
 from Tools.Temperature import get_temperature
 
-pi = False
+pi = True
 if pi:
     import neopixel
     import board
 
-    strip_length = 255
+    strip_length = 256
     s1 = neopixel.NeoPixel(board.D12, strip_length, auto_write=False)
+    s1.brightness = pc.brightness
 
 if pi:
     # fill in borders and what not
@@ -44,56 +45,63 @@ def send_char(dplace, number):
 def send_sec(seconds):
 
     # clear all seconds
-    for p in mat.sec_pix:
+    for p in mat.seconds_mat:
         if pi:
             s1[p] = pc.color_off
 
     # turn on all seconds that on
     for p in range(seconds):
         if pi:
-            s1[mat.sec_pix[p]] = pc.color_sec
+            s1[mat.seconds_mat[p]] = pc.color_sec
 
 
-temp_time = time.perf_counter()
+def clock_mode():
+    temp_time = time.perf_counter()
+    temp = get_temperature()
+    temp1 = int(temp//10)
+    temp2 = int(temp%10)
 
-temp = get_temperature()
-temp1 = int(temp//10)
-temp2 = int(temp%10)
+    while True:
+        if pi:
+            s1.fill([0, 0, 0])
+        tn = datetime.now().strftime("%d %H:%M:%S")
+
+        tc = tn[3], tn[4], tn[6], tn[7]
+        for i, t in enumerate(tc):
+            send_char(i + 1, int(t))
+
+        # determining seconds to send
+        secs = int(tn[9:11])
+        send_sec(secs)
+
+        # day
+        d1 = int(tn[0])
+        d2 = int(tn[1])
+        # temperature
+        et = time.perf_counter() - temp_time
+        if et > pc.temp_refresh:
+            temp_time = time.perf_counter()
+            try:
+                temp = get_temperature()
+                temp1 = int(temp//10)
+                temp2 = int(temp%10)
+            except:
+                temp = 0
+
+        # sending date and temperature
+        for i, d in enumerate([temp1, temp2]):
+            send_char(i + 5, d)
+
+        if pi:
+            s1.show()
+        # date to set
+
+        time.sleep(0.5)
+
 
 while True:
-    if pi:
-        s1.fill([0, 0, 0])
-    tn = datetime.now().strftime("%d %H:%M:%S")
-
-    tc = tn[3], tn[4], tn[6], tn[7]
-    for i, t in enumerate(tc):
-        send_char(i + 1, int(t))
-
-    # determining seconds to send
-    secs = int(tn[6:8])
-    send_sec(secs)
-
-    # day
-    d1 = int(tn[0])
-    d2 = int(tn[1])
-    # temperature
-    et = time.perf_counter() - temp_time
-    if et > pc.temp_refresh:
-        temp_time = time.perf_counter()
-        try:
-            temp = get_temperature()
-            temp1 = int(temp//10)
-            temp2 = int(temp%10)
-        except:
-            temp = 0
-
-    # sending date and temperature
-    for i, d in enumerate([d1, d2, temp1, temp2]):
-        print(d)
-        send_char(i + 4, d)
-
-    if pi:
-        s1.show()
-    # date to set
-
-    time.sleep(0.5)
+    try:
+        clock_mode()
+    except Exception as e:
+        print(f"Exception {e}")
+        time.sleep(10)
